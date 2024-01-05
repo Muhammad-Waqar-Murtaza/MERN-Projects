667d4f7bb42bd4d0932248d0ce5ccd1caedff46b
Muhammad Waqar Murtaza <mwm.waqarmurtaza@gmail.com>
2024-01-05 19:48:49 +0500

    mern-projects

--- MERN-AUTH/server/index.js
+++ MERN-AUTH/server/index.js
@@ -8,11 +8,17 @@ const app = express()
 
 // middleware
 
-app.use(cors({
-    origin: ["https://form-auth-server.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-}))
+app.use((req, res, next) => {
+    //allow access from every, elminate CORS
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.removeHeader('x-powered-by');
+    //set the allowed HTTP methods to be requested
+    res.setHeader('Access-Control-Allow-Methods','POST');
+    //headers clients can use in their requests
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    //allow request to continue and be handled by routes
+    next();
+  });
 app.use(express.json())
 connectDB()
 
