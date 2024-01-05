050995586b51b2da43baa5bdb4ad9c84a5dee9ab
Muhammad Waqar Murtaza <mwm.waqarmurtaza@gmail.com>
2024-01-05 20:22:51 +0500

    mern-projects

--- MERN-AUTH/server/index.js
+++ MERN-AUTH/server/index.js
@@ -8,18 +8,10 @@ const app = express()
 
 // middleware
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    next()
-})
+app.use(cors({
+    origin: "https://mern-projects-iota.vercel.app/",
+    methods: ["POST, GET"],
+}))
 
 app.use(express.json())
 connectDB()
