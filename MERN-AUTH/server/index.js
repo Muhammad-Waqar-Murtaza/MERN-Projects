cac7d6c6793c09721bf972a1d4221397af0092a4
Muhammad Waqar Murtaza <mwm.waqarmurtaza@gmail.com>
2024-01-05 21:41:51 +0500

    mern-projects

--- MERN-AUTH/server/index.js
+++ MERN-AUTH/server/index.js
@@ -16,16 +16,11 @@ const app = express()
 //     next();
 // })
 
-// app.use(cors({
-//     origin: ["https://mern-projects-iota.vercel.app"],
-//     methods: ["POST, GET"],
-//     credentials: true
-// }))
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'Origin');
-    next();
-  });
+app.use(cors({
+    origin: ["https://mern-projects-iota.vercel.app/https://form-auth-server.vercel.app/"],
+    methods: ["POST, GET"],
+    credentials: true
+}))
 
 app.use(express.json())
 connectDB()
