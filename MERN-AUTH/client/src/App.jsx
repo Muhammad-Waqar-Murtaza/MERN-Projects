652c7ba66455c149b3c86d324c07049e5b88c159
Muhammad Waqar Murtaza <mwm.waqarmurtaza@gmail.com>
2024-01-05 19:55:50 +0500

    mern-projects

--- MERN-AUTH/client/src/App.jsx
+++ MERN-AUTH/client/src/App.jsx
@@ -6,11 +6,15 @@ import UserProfile from "./components/Home/UserProfile";
 import Header from "./components/Home/Header";
 import { useState } from "react";
 import NotFound from "./components/NotFound";
+import axios from "axios"
 
 function App() {
   const initialToken = localStorage.getItem('token')
   const [toggle, setToggle] = useState(false)
   const [token, setToken] = useState(initialToken)
+
+  axios.defaults.withCredentials = true
+  
   return (
     <BrowserRouter>
       <Header toggle={toggle} setToggle={setToggle} token={token} setToken={setToken} />
